7fbac45a124454aecc02928590f7c897478fc30f
honeypolarbear <cjkim6375@naver.com>
2022-01-18 12:42:20 +0900

    20220118
    오늘치

--- Hello_Coding/Assets/HelloCode.cs
+++ Hello_Coding/Assets/HelloCode.cs
@@ -7,21 +7,19 @@ public class HelloCode : MonoBehaviour
     
     void Start()
     {
-        { 
-        float distance = GetDistance(2, 2, 5, 6);
-        Debug.Log("(2,2)에서 (5,6)까지의 거리 :" + distance);
-        }
-        
-            float GetDistance(float x1, float y1, float x2, float y2)
-        {
-            float width = x2 - x1;
-            float height = y2 - y1;
+        int[] students = new int[5];
 
-            float distance = width * width + height * height;
-            distance = Mathf.Sqrt(distance);
+        students[0] = 100;
+        students[1] = 90;
+        students[2] = 80;
+        students[3] = 70;
+        students[4] = 60;
 
-            return distance;
-        }
+        Debug.Log(" 0번 학생의 점수:" + students[0]);
+        Debug.Log(" 1번 학생의 점수:" + students[1]);
+        Debug.Log(" 2번 학생의 점수:" + students[2]);
+        Debug.Log(" 3번 학생의 점수:" + students[3]);
+        Debug.Log(" 4번 학생의 점수:" + students[4]);
     }
 
 
